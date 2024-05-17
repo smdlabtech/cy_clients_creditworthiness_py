06b3e0aa11d1f04f6b634823fdf51d0d072ddbc4
Daya smdlabtech <smdlabtech@gmail.com>
2024-05-17 21:15:02 +0200

    MAJ : Include/

--- streamlit_app/.env_app/Include/styles_app.py
+++ streamlit_app/.env_app/Include/styles_app.py
@@ -1,18 +1,16 @@
-# mod_styles_app.py
+# styles_app.py
 import os
+
 
 def input_css(css_file):
     """
-    Charge et applique un fichier CSS à une application Streamlit.
+    Loads and applies a CSS file to a Streamlit application.
     
     Args:
-    css_file (str): Nom du fichier CSS à charger.
+    css_file (str): Name of CSS file to load.
     """
-    # # Chemin du répertoire courant
-    # current_dir = os.path.dirname(os.path.abspath(__file__))
     
     # # Construire le chemin complet vers le fichier CSS en utilisant le nom du fichier passé en argument
-    # css_path = os.path.join(current_dir, "assets/css/", css_file)
     css_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".", "assets", "css", css_file)
 
     with open(css_path) as f:
